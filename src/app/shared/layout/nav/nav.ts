949426234fa867eb507a145c882c9dbb20a4ad25
Nick <nikitareznov@gmail.com>
2026-01-19 18:52:27 +0200

    сеарч поиск по имени

--- src/app/shared/layout/nav/nav.ts
+++ src/app/shared/layout/nav/nav.ts
@@ -2,21 +2,28 @@ import { Component, inject, signal } from '@angular/core';
 import { Container } from '../../container/container';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { CartUiService } from '../../../services/cart-ui';
 import { AuthStore } from '../../../core/services/auth.store';
-
+import { ProductService } from '../../../core/services/products/product.service';
+import { Product } from '../../../core/models/products/product.model';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink, MatIconModule, MatButtonModule, Container],
+  imports: [FormsModule, RouterLink, MatIconModule, MatButtonModule, Container],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   cartUi = inject(CartUiService);
   authStore = inject(AuthStore);
+  private router = inject(Router);
+  private productService = inject(ProductService);
 
+  search = '';
+  results = signal<Product[]>([]);
+  searchOpen = signal(false);
   // состояние дропдауна
   dropdownOpen = signal(false);
   get user() {
@@ -26,7 +33,25 @@ export class Nav {
   get avatarUrl() {
     return this.authStore.avatarUrl();
   }
+  onSearchChange() {
+    if (!this.search.trim()) {
+      this.results.set([]);
+      this.searchOpen.set(false);
+      return;
+    }
 
+    this.productService.getProducts({ search: this.search }).subscribe((res) => {
+      this.results.set(res.slice(0, 5)); // топ 5
+      this.searchOpen.set(true);
+    });
+  }
+
+  goToProduct(p: Product) {
+    this.search = '';
+    this.searchOpen.set(false);
+    this.results.set([]);
+    this.router.navigate(['/product', p.id]);
+  }
   logout() {
     this.authStore.logout();
   }
