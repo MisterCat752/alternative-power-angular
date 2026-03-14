dd3d7c9e56d3797050cfd7baa05ca6d7b259ffdf
Nick <nikitareznov@gmail.com>
2026-03-14 17:41:41 +0200

    fix wish list and top bar

--- src/app/shared/layout/top-bar/top-bar.ts
+++ src/app/shared/layout/top-bar/top-bar.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Container } from '../../container/container';
 import { CategoryService } from '../../../core/services/category.service';
 import { Category } from '../../../core/models/category.model';
-import { RouterLink } from '@angular/router';
+import { Router, NavigationEnd, RouterLink } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 interface MenuCategory extends Category {
   children?: MenuCategory[];
@@ -20,20 +21,45 @@ export class TopBar implements OnInit {
   isOpen = false;
   categories: MenuCategory[] = [];
 
-  constructor(private categoryService: CategoryService) {}
+  constructor(
+    private categoryService: CategoryService,
+    private router: Router,
+    private el: ElementRef,
+  ) {}
 
   ngOnInit() {
     this.categoryService.list().subscribe((cats) => {
       this.categories = this.buildTree(cats);
+    });
+
+    // закрытие при переходе
+    this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe(() => {
+      this.isOpen = false;
     });
   }
 
   toggleMenu() {
     this.isOpen = !this.isOpen;
   }
+
   closeMenu() {
     this.isOpen = false;
   }
+
+  // закрытие при клике вне меню
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: MouseEvent) {
+    if (!this.el.nativeElement.contains(event.target)) {
+      this.isOpen = false;
+    }
+  }
+
+  // закрытие по ESC
+  @HostListener('document:keydown.escape')
+  onEsc() {
+    this.isOpen = false;
+  }
+
   private buildTree(categories: Category[]): MenuCategory[] {
     const map = new Map<number, MenuCategory>();
     const roots: MenuCategory[] = [];
